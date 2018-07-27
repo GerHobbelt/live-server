fdbda25aff6fb98855c76e4d556bed53b50990db
Luke Kaalim <lkaalim@nine.com.au>
2018-07-27 16:42:58 +1000

    Update cli tests to not fail if serving is not at zeroth index
    String is still correct, still displays correct information, but starts from index 5 instead.

--- test/cli.js
+++ test/cli.js
@@ -14,6 +14,7 @@ function exec_test(args, callback) {
 }
 
 describe('command line usage', function() {
+	this.timeout(2000);
 	it('--version', function(done) {
 		exec_test([ "--version" ], function(error, stdout, stdin) {
 			assert(!error, error);
@@ -38,7 +39,7 @@ describe('command line usage', function() {
 	it('--port', function(done) {
 		exec_test([ "--port=16123", "--no-browser", "--test" ], function(error, stdout, stdin) {
 			assert(!error, error);
-			assert(stdout.indexOf("Serving") == 0, "serving string not found");
+			assert(stdout.indexOf("Serving") != -1, "serving string not found");
 			assert(stdout.indexOf("at http://127.0.0.1:16123") != -1, "port string not found");
 			done();
 		});
@@ -46,7 +47,7 @@ describe('command line usage', function() {
 	it('--host', function(done) {
 		exec_test([ "--host=localhost", "--no-browser", "--test" ], function(error, stdout, stdin) {
 			assert(!error, error);
-			assert(stdout.indexOf("Serving") == 0, "serving string not found");
+			assert(stdout.indexOf("Serving") != -1, "serving string not found");
 			assert(stdout.indexOf("at http://localhost:") != -1, "host string not found");
 			done();
 		});
@@ -58,7 +59,7 @@ describe('command line usage', function() {
 				"--test"
 			], function(error, stdout, stdin) {
 			assert(!error, error);
-			assert(stdout.indexOf("Serving") == 0, "serving string not found");
+			assert(stdout.indexOf("Serving") != -1, "serving string not found");
 			done();
 		});
 	});
